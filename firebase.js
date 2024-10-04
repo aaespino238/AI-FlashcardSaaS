b66fbe34afbefdd6385a160f8bfd3d02364d21a6
aaespino238 <77126568+aaespino238@users.noreply.github.com>
2024-10-04 14:07:29 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCttUv3Vj3agBo-cd6p_DJgbDiJsnDwfCI",
-  authDomain: "flashcard-saas-f5a5b.firebaseapp.com",
-  projectId: "flashcard-saas-f5a5b",
-  storageBucket: "flashcard-saas-f5a5b.appspot.com",
-  messagingSenderId: "1082865352072",
-  appId: "1:1082865352072:web:2d4c6662dcfa7baa20ce3d",
-  measurementId: "G-RF45B6MPKM",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
